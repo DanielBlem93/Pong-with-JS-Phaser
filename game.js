9c873d489733fad8044fb0118546e2e04aec4408
Daniel San <106621051+DanielBlem93@users.noreply.github.com>
2024-02-11 18:19:58 +0100

    added simple gameplay loop

--- game.js
+++ game.js
@@ -1,16 +1,19 @@
+
 start = true
+started = false
 class Pong extends Phaser.Scene {
   player1
   player2
   ball
-  ballVelocityX = 200
-  ballVelocityY = (Math.random() *200) -100
+  ballVelocityX = -200
+  ballVelocityY = (Math.random() * 200) - 100
   cursors
   keyW
   keyS
-  player1Score = 0
-  player2Score = 0
-  scoreText = 'score:0'
+  player1Score
+  player2Score
+  score1 = 0
+  score2 = 0
 
 
   constructor() {
@@ -32,6 +35,10 @@ class Pong extends Phaser.Scene {
     this.addPlayers()
     this.addControls()
     this.addBall()
+    this.setCollider()
+    this.onWorldBoundsListener()
+
+
 
   }
 
@@ -40,19 +47,109 @@ class Pong extends Phaser.Scene {
     this.handlePlayerControls(this.player1, this.cursors);
     this.handlePlayerControls(this.player2, { up: this.keyW, down: this.keyS });
     this.startgame()
+    this.holdPlayersPosition()
+    // this.checkWinner()
   }
+
+
+  // checkWinner() {
+  //   if (score1 === 7) {
+
+  //   }
+  // }
+
+  gameOver() {
+    this.physics.pause();
+    setTimeout(() => {
+      this.resetGame()
+      setTimeout(() => {
+        this.startgame()
+        this.physics.resume()
+      }, 2000);
+    }, 2000);
+  }
+  onWorldBoundsListener() {
+    this.physics.world.on('worldbounds', (body, up, down, left, right) => {
+      if (right) {
+        this.addPoints('player1')
+      }
+      else if (left) {
+        this.addPoints('player2')
+      }
+    });
+  }
+
+  resetGame() {
+    // Ballposition zurücksetzen
+    this.ball.setPosition(401, 242.5);
+    this.ballVelocityX = -200
+    this.ballVelocityY = (Math.random() * 200) - 100
+
+    // Spielerpositionen zurücksetzen
+    this.player1.setPosition(770, 247);
+    this.player2.setPosition(30, 247);
+
+    // Spielzustand zurücksetzen
+    start = false;
+    started = false;
+  }
+
+
+  addPoints(player) {
+
+    if (player === 'player1')
+      this.score2++
+    else {
+
+      this.score1++
+    }
+    this.player1Score.setText('' + this.score1)
+    this.player2Score.setText('' + this.score2)
+    this.gameOver()
+  }
+
+
+  setCollider() {
+    this.physics.add.collider(this.player1, this.ball, this.hitBall, null, this);
+    this.physics.add.collider(this.player2, this.ball, this.hitBall, null, this);
+  }
+
+
+  hitBall() {
+    if (this.ballVelocityX > 0) {
+
+      this.ballVelocityX = -(Math.abs(this.ballVelocityX)) - 10
+      this.ball.setVelocityX(this.ballVelocityX)
+
+    } else if (this.ballVelocityX < 0) {
+      console.log(this.ballVelocityX)
+      this.ballVelocityX = (Math.abs(this.ballVelocityX)) + 10
+      this.ball.setVelocityX(this.ballVelocityX)
+
+    }
+  }
+
 
   startgame() {
-
-    this.ball.setVelocityX(this.ballVelocityX)
-    this.ball.setVelocityY(this.ballVelocityY)
-
+    if (start && !started) {
+      this.ball.setVelocityX(this.ballVelocityX)
+      this.ball.setVelocityY(this.ballVelocityY)
+      started = true
+    }
   }
+
+  holdPlayersPosition() {
+    this.player1.x = 770
+    this.player2.x = 30
+  }
+
 
   addBall() {
     this.ball = this.physics.add.sprite(401, 242.5, 'ball');
     this.ball.setCollideWorldBounds(true);
     this.ball.setBounce(1);
+    this.ball.body.onWorldBounds = true;
+    this.ball.body.collideWorldBounds = true
   }
 
   changeWorldBounds() {
